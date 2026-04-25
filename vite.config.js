f843592f23c8ef0fa1447fefb506464989f46221
Yutaro Hirabayashi <yutaro@hirabayashi89.com>
2026-04-25 15:21:58 +0900

    chore: vite-plugin-sass-glob-importを削除（明示的@useリストに移行）

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import sassGlobImports from 'vite-plugin-sass-glob-import';
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   server: {
     open: true
   },
-  plugins: [
-    sassGlobImports()
-  ],
   build: {
     outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true
